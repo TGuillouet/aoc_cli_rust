31bdb28931b6640451c69ad18a188cee165cb954
Thomas Guillouët <thomas.guillouet@outlook.com>
2023-02-16 17:06:50 +0100

    Fixing clippy warnings

--- examples/specific_date/main.rs
+++ examples/specific_date/main.rs
@@ -9,5 +9,10 @@ fn main() {
     let mut days: HashMap<u32, Box<dyn RunnableDay>> = HashMap::new();
     days.insert(1, Box::new(Day01::default()));
 
-    aoc_cli::init(days);
+    let aoc_result = aoc_cli::init(days);
+
+    match aoc_result {
+        Ok(_) => println!("Execution finished"),
+        Err(error) => eprintln!("Error while executing this day: {}", error),
+    }
 }
