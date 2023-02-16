9c8db74aa505f1ad44db92c008c7377a1ea34a92
Thomas Guillouët <thomas.guillouet@outlook.com>
2023-02-16 11:19:14 +0100

    day 1 parsing lines

--- examples/specific_date/day_01.rs
+++ examples/specific_date/day_01.rs
@@ -5,17 +5,15 @@ pub struct Day01 {}
 
 impl RunnableDay for Day01 {
     fn part_1(&self, data: Vec<String>) -> i32 {
-        println!("Part 1");
         0
     }
 
     fn part_2(&self, data: Vec<String>) -> i32 {
-        println!("Part 2");
         0
     }
 
     fn parse_input(&self, lines: Vec<String>) -> Vec<String> {
-        println!("Input");
-        vec![]
+        let lines: Vec<String> = lines[0].split(",").map(|i| i.to_owned()).collect();
+        lines
     }
 }
